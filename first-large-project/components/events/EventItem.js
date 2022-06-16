0827ce3394daed5e2a2feb53e8a547ab83abb19c
ArtJSON <artur.pietrzak1@outlook.com>
2022-06-16 18:05:08 +0200

    Add icons components

--- first-large-project/components/events/EventItem.js
+++ first-large-project/components/events/EventItem.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import classes from "./EventItem.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/DateIcon";
+import AddressIcon from "../icons/AddressIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -22,14 +25,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{displayDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{displayLocation}</address>
           </div>
         </div>
         <div className={classes.action}>
-          <Button link={exploreLink}>Explore event</Button>
+          <Button link={exploreLink}>
+            <span>Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
